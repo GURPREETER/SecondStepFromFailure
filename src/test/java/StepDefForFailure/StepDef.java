fdeb5b121878d90b439240915fce0c36d6870f66
Gurpreet Kaur <ergurpreet587@gmail.com>
2020-04-05 23:02:25 +0100

    Output to check on console

--- src/test/java/StepDefForFailure/StepDef.java
+++ src/test/java/StepDefForFailure/StepDef.java
@@ -15,11 +15,11 @@ public class StepDef {
     @Given("^This is the login page$")
     public void this_is_the_login_page() throws Throwable {
         // Write code here that turns the phrase above into concrete actions
-        System.setProperty("webdriver.chrome.driver","C:/Users/gkgur/ChromeSetup/chromedriver.exe");
-        WebDriver fb = new ChromeDriver();
-        fb.manage().window().maximize();
-        fb.get("https://www.linkedin.com");
-
+//        System.setProperty("webdriver.chrome.driver","C:/Users/gkgur/ChromeSetup/chromedriver.exe");
+//        WebDriver fb = new ChromeDriver();
+//        fb.manage().window().maximize();
+//        fb.get("https://www.linkedin.com");
+        System.out.println("Hello world Here it is running !!!");
 
     }
 
